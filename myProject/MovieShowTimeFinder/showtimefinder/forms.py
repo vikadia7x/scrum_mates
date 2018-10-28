f3236944f558f9ea9acb48e5edb931a7dfdfd3f0
Shivam Verma <shivamverma2392@gmail.com>
2018-10-28 14:04:16 -0700

    User Story: 80
    Task 99:
    
    - Created a page select.html for user to select the genre of their preference.
    
    - Created a function select in veiws.py to get the user response on the selected genre.
    
    - Created a list of the movies falling in those genre to be display onn the page after the select genre page.
    
    Task 100:
    
    - Created a page for user to select movies after selecting genre.
    
    - The page displays the list top rated movies of the genre selected.
    
    - Directed the user to the home after storing the preference.
    
    Task 101:
    
    - Updated user profile model and added isFirstLogin flag to identify users if they are logging in for the first time.
    
    - Updated the flag to 0 after the user has seleted the genre and movie preferences.
    
    Task 102:
    
    - Created UserSelection model to store movie preferece of a user.
    
    - Created MovieGenreSelection to display genre list to the user.
    
    - Created a for MovieGenreSelection to get the user response.

--- myProject/MovieShowTimeFinder/showtimefinder/forms.py
+++ myProject/MovieShowTimeFinder/showtimefinder/forms.py
@@ -16,3 +16,28 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username','first_name','last_name','email','dateofbirth','zipcode','password1', 'password2', )
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+
+class MovieSelection(forms.Form):
+    Action = forms.BooleanField(required=False)
+    Adventure = forms.BooleanField(required=False)
+    Animation = forms.BooleanField(required=False)
+    Comedy = forms.BooleanField(required=False)
+    Crime = forms.BooleanField(required=False)
+    Documentary = forms.BooleanField(required=False)
+    Drama = forms.BooleanField(required=False)
+    Family = forms.BooleanField(required=False)
+    Fantasy = forms.BooleanField(required=False)
+    History = forms.BooleanField(required=False)
+    Horror = forms.BooleanField(required=False)
+    Music = forms.BooleanField(required=False)
+    Mystery = forms.BooleanField(required=False)
+    Romance = forms.BooleanField(required=False)
+    Science = forms.BooleanField(required=False)
+    TV = forms.BooleanField(required=False)
+    Thriller = forms.BooleanField(required=False)
+    War = forms.BooleanField(required=False)
+    Western = forms.BooleanField(required=False)
